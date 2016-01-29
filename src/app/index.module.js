87bd99ae408d7b84e15415f5b0b3aa328a0c252c
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2016-01-29 11:35:14 +0800

    init

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('angularTutorial', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngResource', 'ui.router', 'toastr']);
+
+})();
