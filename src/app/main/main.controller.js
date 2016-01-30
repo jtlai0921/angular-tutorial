c88ce6a3923e12bbdcb45ac1a7e8f1de948292b0
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2016-01-30 16:49:39 +0800

    update main controller

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,38 +6,10 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($state, $timeout, webDevTec, toastr, auth) {
+  function MainController($state, auth) {
     if (!auth.isLoggedIn()) {
       $state.go('auth');
       return;
-    }
-    var vm = this;
-
-    vm.awesomeThings = [];
-    vm.classAnimation = '';
-    vm.creationDate = 1454037671490;
-    vm.showToastr = showToastr;
-
-    activate();
-
-    function activate() {
-      getWebDevTec();
-      $timeout(function() {
-        vm.classAnimation = 'rubberBand';
-      }, 4000);
-    }
-
-    function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-      vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
     }
   }
 })();
--- src/app/main/main.controller.spec.js
+++ src/app/main/main.controller.spec.js
@@ -1,39 +1,24 @@
 (function() {
   'use strict';
 
-  describe('controllers', function(){
-    var vm;
-    var $timeout;
-    var toastr;
+  fdescribe('controllers', function(){
+    var auth, $state, $controller;
 
     beforeEach(module('angularTutorial'));
-    beforeEach(inject(function(_$controller_, _$timeout_, _webDevTec_, _toastr_) {
-      spyOn(_webDevTec_, 'getTec').and.returnValue([{}, {}, {}, {}, {}]);
-      spyOn(_toastr_, 'info').and.callThrough();
-
-      vm = _$controller_('MainController');
-      $timeout = _$timeout_;
-      toastr = _toastr_;
+    beforeEach(inject(function(_$controller_, _auth_, _$state_) {
+      $state = _$state_;
+      auth = _auth_
+      $controller = _$controller_;
     }));
 
-    it('should have a timestamp creation date', function() {
-      expect(vm.creationDate).toEqual(jasmine.any(Number));
+    it('goes to `auth` state if not logged in', function(){
+      spyOn($state, 'go');
+      spyOn(auth, 'isLoggedIn').and.returnValue(false);
+
+      $controller('MainController');
+
+      expect($state.go).toHaveBeenCalledWith('auth');
     });
 
-    it('should define animate class after delaying timeout ', function() {
-      $timeout.flush();
-      expect(vm.classAnimation).toEqual('rubberBand');
-    });
-
-    it('should show a Toastr info and stop animation when invoke showToastr()', function() {
-      vm.showToastr();
-      expect(toastr.info).toHaveBeenCalled();
-      expect(vm.classAnimation).toEqual('');
-    });
-
-    it('should define more than 5 awesome things', function() {
-      expect(angular.isArray(vm.awesomeThings)).toBeTruthy();
-      expect(vm.awesomeThings.length === 5).toBeTruthy();
-    });
   });
 })();
